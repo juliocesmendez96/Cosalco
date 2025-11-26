c76827f0031daa7fcbbc35f85c05d1c532ea0d03
Julio <juliocesmendez96@gmail.com>
2025-11-26 20:21:53 -0300

    fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from 'https://skqjginecjtcsknnddwc.supabase.co';
 
 const supabase = createClient('https://skqjginecjtcsknnddwc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrcWpnaW5lY2p0Y3Nrbm5kZHdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNzYzNjksImV4cCI6MjA3OTY1MjM2OX0.E5K6P4fBGYnrlo8cdNYx5JsJQhlSO8ndtJHnGbTbuL8');
 
