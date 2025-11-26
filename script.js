141834bcf6cc9edd03f10e27f1a5d07a0ba736bd
Julio <juliocesmendez96@gmail.com>
2025-11-26 20:18:42 -0300

    fix

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 const supabase = createClient('https://skqjginecjtcsknnddwc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrcWpnaW5lY2p0Y3Nrbm5kZHdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNzYzNjksImV4cCI6MjA3OTY1MjM2OX0.E5K6P4fBGYnrlo8cdNYx5JsJQhlSO8ndtJHnGbTbuL8');
 
 async function loadProducts() {
-    let { data, error } = await supabase
-    .from('Articulos') // probá con el nombre exacto
-    .select('*')
-    .limit(5);
+    console.log("Cargando datos...");
 
-    console.log("DATA:", data);
-    console.log("ERROR:", error);
+    const { data, error } = await supabase
+        .from('Articulos')
+        .select('*')
+        .limit(50);
 
+    if (error) {
+        console.error("ERROR:", error);
+        return;
+    }
+
+    console.log("RESULTADOS:", data);
 }
+
 loadProducts();
